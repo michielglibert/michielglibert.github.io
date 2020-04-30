44dd0461008ab5f3f679c4767de52dc550117d34
Michiel <michiel.glibert@gmail.com>
2020-05-01 01:02:58 +0200

    Fix image

--- portfolio_final/artifacts/react-static-templates.js
+++ portfolio_final/artifacts/react-static-templates.js
@@ -1,27 +1,10 @@
 
 
-import React from 'react'
-import universal, { setHasBabelPlugin } from '/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-universal-component/dist/index.js'
-
-setHasBabelPlugin()
-
-const universalOptions = {
-  loading: () => null,
-  error: props => {
-    console.error(props.error);
-    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
-  },
-  ignoreBabelRename: true
-}
-
-const t_0 = universal(import('/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404'), universalOptions)
-      t_0.template = '/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404'
-      
-
+  
 // Template Map
 export default {
-  '/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404': t_0
+  '/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404': require('/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404').default
 }
-// Not Found Template
-export const notFoundTemplate = "/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404"
+
+export const notFoundTemplate = '/mnt/c/Users/michi/Projects/michielglibert.github.io/portfolio_final/node_modules/react-static/lib/browser/components/Default404'
 
